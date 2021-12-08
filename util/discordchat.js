ce0ba21564089c0ebae8f007fbf3cf092d1f73df
kat <34391969+kat0154@users.noreply.github.com>
2021-12-08 18:49:41 -0500

    Update discordchat.js

--- util/discordchat.js
+++ util/discordchat.js
@@ -1,6 +1,5 @@
-//https://discord.com/api/webhooks/917962175656636457/'aAsYjjN3Oa1QZ84Hvs4Keuk-9xagj41krH0jrTlJUab-2UPg-pIg1bcu-jrYGm8hhgaD'
 const Discord = require('discord.js');
-const webhookClient = new Discord.WebhookClient({id: '917962175656636457', token: 'aAsYjjN3Oa1QZ84Hvs4Keuk-9xagj41krH0jrTlJUab-2UPg-pIg1bcu-jrYGm8hhgaD'});
+const webhookClient = new Discord.WebhookClient({id: 'webhook-id', token: 'webhook-token'});
 
 module.exports = {
     run: async (client, channel, tags, message) => {
