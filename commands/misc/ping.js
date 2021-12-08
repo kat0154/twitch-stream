5dcab472df86ddae31a3de70f5d789a01ff30216
kat <34391969+kat0154@users.noreply.github.com>
2021-12-08 18:46:13 -0500

    Delete ping.js

--- commands/misc/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    help: {
-        name: 'ping'
-    },
-    run: async (client, channel, tags, message) => {
-        client.say(channel, `@${tags.username}, PONG!`);
-    }
-};
